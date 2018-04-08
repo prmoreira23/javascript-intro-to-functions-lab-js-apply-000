3fe68a35bf184148ed40c4d0c9653117cf67d319
Pablo Rocha Moreira <prm.gredes@gmail.com>
2018-04-08 06:46:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,5 +15,5 @@ function logWhisper(text){
 }
 
 function sayHiToGrandma(text){
-    return text === text.toLowerCase() ? "I can't hear you!" : "";
+    return text === text.toLowerCase() ? "I can't hear you!" : "YES INDEED!";
 }
